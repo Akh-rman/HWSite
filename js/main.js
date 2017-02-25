70c7b63319da88000c2d7d924926eaa21068c64a
Rman <rman@Mac-Rman.local>
2017-02-25 20:09:43 +0300

    Создал функцию changeDiv, которая изменяет область ответа игр 'Угадайка' и 'Угадайка-мультиплеер' в зависимости от того правильный ответ

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ $(function () { //Функция для сворачивания меню ког
         }
     });
 });
+
+function changeDiv(color) { //функция изменения области ответа игры "Угадайка" и "Угадайка-мультиплеер"
+    var answer = document.getElementById("answer");
+    answer.style.height = "70px";
+    answer.style.width = "250px";
+    answer.style.margin = "20px auto";
+    answer.style.display = "flex";
+    
+    if (color == "red") {
+        answer.style.background = "#ff1b1b";
+        answer.style.boxShadow = "0 0 10px 4px #ff1b1b";
+        answer.style.animation = "background_red 1s linear 1s infinite";
+    }
+    else if (color == "green") {
+        answer.style.background = "#079507";
+        answer.style.boxShadow = "0 0 10px 4px #079507";
+        answer.style.animation = "background_green 2s linear 1s 1";
+    }
+}
