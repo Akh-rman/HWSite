b790848d546fd063cab1d8e7a3e8f6608c2cf29e
Rman <rman@Mac-Rman.local>
2017-04-10 17:24:22 +0300

    Создал папку en. Папка будет содержать английскую версию сайта. Перенес в папку файлы.

---
+++ en/js/main.js
@@ -0,0 +1,51 @@
+$(function() { //Функция для сворачивания меню когда фокус переходит на другие элементы
+    $(".navbar-toggle").blur(function (event) {
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#bs-example-navbar-collapse-1").collapse('hide');
+        }
+    });
+});
+
+var body = document.querySelector("body");
+var a = document.createElement("a");
+var span = document.createElement("span");
+addEventListener("scroll", scroll);
+
+function changeButton() { //функция изменения кнопки "вверх"
+    body.appendChild(a);
+    a.className = "scroll-button";
+    a.style.display = "block";
+    a.href = "#header-nav";
+    a.appendChild(span);
+    span.className = "glyphicon glyphicon-chevron-up scroll-span";
+}
+
+function scroll() { //функция для появления кнопки "вверх" при скролле вниз 
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 100) {
+        changeButton();
+    }
+    else {
+        a.style.display = "none";
+    }   
+}
+
+function changeDiv(color) { //функция изменения области ответа игры "Угадайка" и "Угадайка-мультиплеер"
+    var answer = document.getElementById("answer");
+    answer.style.height = "70px";
+    answer.style.width = "250px";
+    answer.style.margin = "20px auto";
+    answer.style.display = "flex";
+    
+    if (color == "red") {
+        answer.style.background = "#ff1b1b";
+        answer.style.boxShadow = "0 0 10px 4px #ff1b1b";
+        answer.style.animation = "background_red 1s linear 1s infinite";
+    }
+    else if (color == "green") {
+        answer.style.background = "#079507";
+        answer.style.boxShadow = "0 0 10px 4px #079507";
+        answer.style.animation = "background_green 2s linear 1s 1";
+    }
+}
---
+++ en/js/quess.js
@@ -0,0 +1,65 @@
+var p = document.getElementsByClassName("text");
+var text = p[0];
+var input = document.createElement("input");
+input.placeholder = "Введите число";
+input.id = "input";
+var div = document.getElementById("quess");
+var button = document.getElementById("start");
+var Numb = {};
+var i = 0;
+button.addEventListener("click", start);
+//console.log(Numb);
+
+function start() {
+    Numb.NumbComp = Math.round(Math.random() * 100);
+    div.insertBefore(input, div.children[1]);
+    button.innerHTML = "Отправить";
+    button.addEventListener("click", playGuess);
+    input.addEventListener("keydown", keyDown);
+    button.removeEventListener("click", start);
+}
+
+function keyDown(event) {
+    if (event.keyCode == 13) {
+        playGuess(); 
+    }
+}
+
+function playGuess() { 
+    var NumbUser = document.getElementById("input").value;
+    NumbUser = parseInt(NumbUser);
+    if (NumbUser > Numb.NumbComp) {
+        i++;
+        //console.log("Раунд " + i + ". Ваше число больше загаданного");
+        text.innerHTML = "Раунд " + i + ". Ваше число больше загаданного";
+        changeDiv("red");
+    }
+				
+    else if (NumbUser < Numb.NumbComp) {
+        i++;
+        //console.log("Раунд " + i + ". Ваше число меньше загаданного");
+        text.innerHTML = "Раунд " + i + ". Ваше число меньше загаданного";
+        changeDiv("red");
+    }
+				
+    else if (NumbUser == Numb.NumbComp) {
+        //console.log("Вы угадали число!");
+        text.innerHTML = "Вы угадали число!";
+        changeDiv("green");
+        button.removeEventListener("click", playGuess);
+        input.removeEventListener("keydown", keyDown);
+    }    
+    else {
+        i++;
+        //console.log("Раунд " + i + ". Ошибка");
+        changeDiv("red");
+        text.innerHTML = "Раунд " + i + ". Ошибка";
+    }                
+    if (i == 10) {
+        answer.style.height = "100px";
+        //console.log("Вы проиграли :( Компьютер загадал число " + Numb.NumbComp);
+        text.innerHTML = "Вы проиграли :( Компьютер загадал число " + Numb.NumbComp;
+        button.removeEventListener("click", playGuess);
+        input.removeEventListener("keydown", keyDown);
+    } 
+}
